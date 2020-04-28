aca2a115216157f01d08c4bf1fde6a347f2ee994
manjaro-pc <1632083718@qq.com>
2020-04-28 16:42:33 +0800

    CLEAN: error_log_handler

--- src/index.cdn.js
+++ src/index.cdn.js
@@ -2,10 +2,9 @@ import App from '@/app'
 import router from '@/router'
 import store from '@/store'
 import '@/router/permission_checker'
-import '@/error_log_handler'
 import '@/css/main.css'
-import PopConfirm from '@c/popconfirm'
 
+import PopConfirm from '@c/popconfirm'
 PopConfirm.install(Vue)
 
 new Vue({
--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import Vue from 'vue'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+globalThis.ELEMENT = ElementUI
 
 import App from '@/app'
 import router from '@/router'
 import store from '@/store'
 import '@/router/permission_checker'
-import '@/error_log_handler'
 import '@/css/main.css'
-
-Vue.use(ElementUI)
-
-globalThis.ELEMENT = ElementUI
 
 new Vue({
 	el: '#app',
